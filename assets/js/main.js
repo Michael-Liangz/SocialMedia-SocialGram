3d147fb0a5e49e07b001c9f62ebc630dd03eb8a1
Michael-Liangz <Michaelliangz.ml@gmail.com>
2023-01-30 19:32:49 +0700

    updated

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+// TODO =============== SIDEBAR ===============
+const menuItems = document.querySelectorAll(".menu-item");
+
+// * Remove active classes from all menu items
+const changeActiveItem = () => {
+  menuItems.forEach((item) => {
+    item.classList.remove("active");
+  });
+};
+
+menuItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    changeActiveItem();
+    item.classList.add("active");
+    if (item.id != "notifications") {
+      document.querySelector(".notifications-popup").style.display = "none";
+    } else {
+      document.querySelector(".notifications-popup").style.display = "block";
+      document.querySelector(
+        "#notifications .notification-count"
+      ).style.display = "none";
+    }
+  });
+});
