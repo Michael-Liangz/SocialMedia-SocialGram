edf21dac498bbb41db6fec7f69bd5d2597987d9e
Michael-Liangz <Michaelliangz.ml@gmail.com>
2023-01-30 21:19:17 +0700

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,7 +26,10 @@ menuItems.forEach((item) => {
 // TODO =============== MESSAGES ===============
 const messagesNotification = document.querySelector("#messages-notification");
 const messages = document.querySelector(".messages");
+const message = messages.querySelectorAll(".message");
+const messageSearch = document.getElementById("message-search");
 
+// * Highlight messages card when icon message at sidebar is clicked
 messagesNotification.addEventListener("click", () => {
   messages.style.boxShadow = "0 0 1rem var(--color-primary)";
   messagesNotification.querySelector(".notification-count").style.display =
@@ -36,3 +39,18 @@ messagesNotification.addEventListener("click", () => {
     messages.style.boxShadow = "none";
   }, 2000);
 });
+
+// * Searching for messages
+const searchMessage = () => {
+  const val = messageSearch.value.toLowerCase();
+  message.forEach((chat) => {
+    let name = chat.querySelector("h5").textContent.toLowerCase();
+    if (name.indexOf(val) != -1) {
+      chat.style.display = "flex";
+    } else {
+      chat.style.display = "none";
+    }
+  });
+};
+// * Search for messages
+messageSearch.addEventListener("keyup", searchMessage);
